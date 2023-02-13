d35bf91ced82e0e1b24d03e9e86e72589110b6b5
tartaric_acid <baka943@qq.com>
2023-02-13 21:14:22 +0800

    部分内容优化 - YSM 格式模型加密再优化，把秘钥和文件名进行加密 - ProGuard 配置文件整理优化，现在更加稳健清晰

--- proguard.pro
+++ proguard.pro
@@ -1,20 +1,36 @@
--dontwarn
+# 导入相关库文件，丢失会导致混淆/加载失败
+-libraryjars <java.home>/jmods/java.base.jmod(!**.jar;!module-info.class)
+-libraryjars libs/forge-1.16.5-36.2.34_mapped_parchment_2022.03.06-1.16.5.jar
+-libraryjars libs/gson-2.8.0.jar
+
+# 打印更加详细的信息，但是忽略依赖错误
+-verbose
+-ignorewarnings
+
+# 不要缩减和优化，否则会导致游戏加载失败
 -dontshrink
 -dontoptimize
--repackageclasses com.elfmcys.yesstevemodel
 
+# 使用指定名称的混淆
 -obfuscationdictionary dict.txt
 -packageobfuscationdictionary dict.txt
 -classobfuscationdictionary dict.txt
+-repackageclasses com.elfmcys.yesstevemodel
 
+# 保留唯一的主模组类
 -keep class com.elfmcys.yesstevemodel.YesSteveModel
--keepclassmembers class * extends java.lang.Enum {
-    <fields>;
+
+# 保留部分枚举类属性
+-keepclassmembers enum * {
     public static **[] values();
     public static ** valueOf(java.lang.String);
 }
+
+# 保留所有用 @Keep 注解标记的方法或者变量
 -keepclassmembers class * {
     @com.elfmcys.yesstevemodel.util.Keep <fields>;
     @com.elfmcys.yesstevemodel.util.Keep <methods>;
 }
+
+# 保留异常、内部类、注解、行数等信息
 -keepattributes Exceptions,InnerClasses,Signature,Deprecated,LineNumberTable,*Annotation*,Synthetic,EnclosingMethod,EventHandler,Override
--- src/main/java/com/elfmcys/yesstevemodel/util/YesModelUtils.java
+++ src/main/java/com/elfmcys/yesstevemodel/util/YesModelUtils.java
@@ -3,7 +3,6 @@ package com.elfmcys.yesstevemodel.util;
 import com.elfmcys.yesstevemodel.model.ServerModelManager;
 import com.google.common.collect.Maps;
 import it.unimi.dsi.fastutil.bytes.ByteArrays;
-import net.minecraft.util.ResourceLocation;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.filefilter.FileFileFilter;
 import org.apache.commons.lang3.tuple.Pair;
@@ -18,10 +17,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.security.GeneralSecurityException;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Map;
+import java.util.*;
 import java.util.zip.DataFormatException;
 
 public final class YesModelUtils {
@@ -31,11 +27,14 @@ public final class YesModelUtils {
      * YSGP 就是 Ying Su Group，映素小组的缩写
      */
     public static final int HEAD = 0x59_53_47_50;
-
     /**
      * 二进制文件的版本号
      */
     public static final int VERSION = 0x00_00_00_01;
+    /**
+     * 新版本号，用了更新的加密解密方法
+     */
+    public static final int VERSION_II = 0x00_00_00_02;
     /**
      * 加密方法
      */
@@ -52,7 +51,7 @@ public final class YesModelUtils {
         if (head != HEAD) {
             return Collections.emptyMap();
         }
-        if (version != VERSION) {
+        if (version != VERSION && version != VERSION_II) {
             return Collections.emptyMap();
         }
 
@@ -66,7 +65,12 @@ public final class YesModelUtils {
         ByteArrayInputStream tmp = new ByteArrayInputStream(modelFilesData);
         while (tmp.available() > 0) {
             try {
-                Pair<String, byte[]> ysmFileData = ysmToFile(tmp);
+                Pair<String, byte[]> ysmFileData;
+                if (version == VERSION) {
+                    ysmFileData = ysmToFile(tmp);
+                } else {
+                    ysmFileData = ysmToFileNew(tmp);
+                }
                 outputs.put(ysmFileData.getKey(), ysmFileData.getValue());
             } catch (GeneralSecurityException | DataFormatException e) {
                 e.printStackTrace();
@@ -96,9 +100,33 @@ public final class YesModelUtils {
         return Pair.of(name, rawData);
     }
 
+    @Nonnull
+    private static Pair<String, byte[]> ysmToFileNew(ByteArrayInputStream tmp) throws IOException, GeneralSecurityException, DataFormatException {
+        String fileName = readBase64String(tmp);
+        int fileSize = readInt(tmp);
+        int cipherSecretKeySize = readInt(tmp);
+
+        byte[] cipherSecretKey = new byte[cipherSecretKeySize];
+        byte[] ivBytes = new byte[16];
+        byte[] fileData = new byte[fileSize];
+        tmp.read(cipherSecretKey);
+        tmp.read(ivBytes);
+        tmp.read(fileData);
+
+        byte[] keyFromMd5 = getKeyFromMd5(fileData);
+        SecretKey secretSecretKey = AESUtil.getKey(keyFromMd5);
+        IvParameterSpec iv = new IvParameterSpec(ivBytes);
+        byte[] decryptSecretKey = AESUtil.decrypt(secretSecretKey, iv, cipherSecretKey).toByteArray();
+        SecretKey key = AESUtil.getKey(decryptSecretKey);
+        ByteArrayOutputStream decryptData = AESUtil.decrypt(key, iv, fileData);
+        byte[] rawData = DeflateUtil.decompressBytes(decryptData.toByteArray());
+
+        return Pair.of(fileName, rawData);
+    }
+
     public static void export(File dir) throws IOException {
         String dirName = dir.getName();
-        if (!ResourceLocation.isValidResourceLocation(dirName)) {
+        if (!ServerModelManager.isValidResourceLocation(dirName)) {
             return;
         }
         boolean noMainModelFile = true;
@@ -141,7 +169,7 @@ public final class YesModelUtils {
     private static byte[] filesToYsm(Collection<File> files) throws IOException {
         ByteArrayOutputStream output = new ByteArrayOutputStream();
         output.write(ByteInteger.int2Bytes(HEAD));
-        output.write(ByteInteger.int2Bytes(VERSION));
+        output.write(ByteInteger.int2Bytes(VERSION_II));
 
         byte[] filesData = filesToBytes(files);
         byte[] md5 = Md5Utils.md5(filesData);
@@ -171,18 +199,49 @@ public final class YesModelUtils {
     private static byte[] fileToBytes(File file) throws IOException, GeneralSecurityException {
         byte[] rawData = FileUtils.readFileToByteArray(file);
         byte[] compressData = DeflateUtil.compressBytes(rawData);
-
+        // 生成秘钥 1 和特征矩阵
         SecretKey secretKey = AESUtil.generateKey();
         IvParameterSpec iv = AESUtil.generateIv();
+        // 将原文件 AES 加密
         ByteArrayOutputStream encryptData = AESUtil.encrypt(secretKey, iv, compressData);
-
+        // 通过文件的 MD5 来生成特殊的秘钥 2
+        byte[] keyFromMd5 = getKeyFromMd5(encryptData.toByteArray());
+        // 用这个秘钥 2 来加密秘钥 1，获得密文秘钥
+        ByteArrayOutputStream cipherSecretKey = AESUtil.encrypt(AESUtil.getKey(keyFromMd5), iv, secretKey.getEncoded());
+        // 开始写文件
         ByteArrayOutputStream output = new ByteArrayOutputStream();
-        writeString(output, file.getName());
+        // 书写 Base64 加密过的字符串
+        writeBase64String(output, file.getName());
+        // 书写密文长度
         output.write(ByteInteger.int2Bytes(encryptData.size()));
-        output.write(secretKey.getEncoded());
+        // 书写密文秘钥长度
+        output.write(ByteInteger.int2Bytes(cipherSecretKey.size()));
+        // 书写密文秘钥
+        output.write(cipherSecretKey.toByteArray());
+        // 书写特征矩阵
         output.write(iv.getIV());
+        // 书写密文
         output.write(encryptData.toByteArray());
         return output.toByteArray();
+    }
+
+    private static byte[] getKeyFromMd5(byte[] fileData) {
+        // 获取得到 MD5
+        byte[] md5 = Md5Utils.md5(fileData);
+        // 以 MD5 为种子，获取固定的随机数
+        Random random = new Random(toLong(md5));
+        // 得到固定的随机数秘钥
+        byte[] keys = new byte[16];
+        random.nextBytes(keys);
+        return keys;
+    }
+
+    private static long toLong(byte[] bytes) {
+        long value = 0;
+        for (byte b : bytes) {
+            value = (value << 8) + (b & 255);
+        }
+        return value;
     }
 
     private static void writeString(ByteArrayOutputStream stream, String string) throws IOException {
@@ -191,11 +250,24 @@ public final class YesModelUtils {
         stream.write(stringBytes);
     }
 
+    private static void writeBase64String(ByteArrayOutputStream stream, String string) throws IOException {
+        byte[] stringBytes = Base64.getEncoder().encode(string.getBytes(StandardCharsets.UTF_8));
+        stream.write(ByteInteger.int2Bytes(stringBytes.length));
+        stream.write(stringBytes);
+    }
+
     private static String readString(ByteArrayInputStream stream) throws IOException {
         int size = readInt(stream);
         byte[] stringBytes = new byte[size];
         stream.read(stringBytes);
-        return new String(stringBytes);
+        return new String(stringBytes, StandardCharsets.UTF_8);
+    }
+
+    private static String readBase64String(ByteArrayInputStream stream) throws IOException {
+        int size = readInt(stream);
+        byte[] stringBytes = new byte[size];
+        stream.read(stringBytes);
+        return new String(Base64.getDecoder().decode(stringBytes), StandardCharsets.UTF_8);
     }
 
     private static boolean readBoolean(ByteArrayInputStream stream) throws IOException {
