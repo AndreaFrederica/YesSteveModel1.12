6b571eeb9bcaeb51c92dda66d3918e68373e7e72
RuiXuqi <90179819+RuiXuqi@users.noreply.github.com>
2026-03-28 15:25:00 +0800

    清理 Layer 结构

--- src/main/java/com/elfmcys/yesstevemodel/client/renderer/layer/CustomPlayerElytraLayer.java
+++ src/main/java/com/elfmcys/yesstevemodel/client/renderer/layer/CustomPlayerElytraLayer.java
@@ -32,35 +32,34 @@ public class CustomPlayerElytraLayer<T extends EntityLivingBase, R extends IGeoR
 
     @Override
     public void render(@Nonnull T livingEntity, float pLimbSwing, float pLimbSwingAmount, float pPartialTicks, float pAgeInTicks, float pNetHeadYaw, float pHeadPitch, Color renderColor) {
-        if (ElytraCompat.isWearingElytra(livingEntity) && this.entityRenderer.getGeoModel() != null) {
-            GeoModel geoModel = this.entityRenderer.getGeoModel();
-            if (!geoModel.elytraBones.isEmpty()) {
-                GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
-                GlStateManager.enableBlend();
-                GlStateManager.blendFunc(GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ZERO);
+        if (!ElytraCompat.isWearingElytra(livingEntity)) return;
+        GeoModel geoModel = this.entityRenderer.getGeoModel();
+        if (geoModel == null || geoModel.elytraBones.isEmpty()) return;
 
-                GlStateManager.pushMatrix();
-                boolean scaleResult = translateToElytra(geoModel);
-                // 缩放不为 0 才会渲染
-                if (!scaleResult) {
-                    GlStateManager.rotate(180, 0, 0, 1);
-                    Minecraft mc = Minecraft.getMinecraft();
-                    mc.getTextureManager().bindTexture(getElytraTexture(livingEntity));
-                    final float scale = 1 / 16F;
-                    this.elytraModel.setRotationAngles(pLimbSwing, pLimbSwingAmount, pAgeInTicks, pNetHeadYaw, pHeadPitch, scale, livingEntity);
-                    this.elytraModel.render(livingEntity, pLimbSwing, pLimbSwingAmount, pAgeInTicks, pNetHeadYaw, pHeadPitch, scale);
-                    ItemStack stack = livingEntity.getItemStackFromSlot(EntityEquipmentSlot.CHEST);
-                    if (stack.hasEffect()) {
-                        RenderPlayer renderer = mc.getRenderManager().getSkinMap().get("default");
-                        LayerArmorBase.renderEnchantedGlint(renderer, livingEntity, this.elytraModel, pLimbSwing, pLimbSwingAmount, pPartialTicks, pAgeInTicks, pNetHeadYaw, pHeadPitch, scale);
-                    }
-                }
-                GlStateManager.popMatrix();
+        GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
+        GlStateManager.enableBlend();
+        GlStateManager.blendFunc(GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ZERO);
 
-                GlStateManager.disableBlend();
-                GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
+        GlStateManager.pushMatrix();
+        boolean scaleResult = RenderUtils.prepMatrixForLocator(geoModel.elytraBones);
+        // 缩放不为 0 才会渲染
+        if (!scaleResult) {
+            GlStateManager.rotate(180, 0, 0, 1);
+            Minecraft mc = Minecraft.getMinecraft();
+            mc.getTextureManager().bindTexture(getElytraTexture(livingEntity));
+            final float scale = 1 / 16F;
+            this.elytraModel.setRotationAngles(pLimbSwing, pLimbSwingAmount, pAgeInTicks, pNetHeadYaw, pHeadPitch, scale, livingEntity);
+            this.elytraModel.render(livingEntity, pLimbSwing, pLimbSwingAmount, pAgeInTicks, pNetHeadYaw, pHeadPitch, scale);
+            ItemStack stack = livingEntity.getItemStackFromSlot(EntityEquipmentSlot.CHEST);
+            if (stack.hasEffect()) {
+                RenderPlayer renderer = mc.getRenderManager().getSkinMap().get("default");
+                LayerArmorBase.renderEnchantedGlint(renderer, livingEntity, this.elytraModel, pLimbSwing, pLimbSwingAmount, pPartialTicks, pAgeInTicks, pNetHeadYaw, pHeadPitch, scale);
             }
         }
+        GlStateManager.popMatrix();
+
+        GlStateManager.disableBlend();
+        GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
     }
 
     protected static ResourceLocation getElytraTexture(EntityLivingBase livingEntity) {
@@ -72,9 +71,5 @@ public class CustomPlayerElytraLayer<T extends EntityLivingBase, R extends IGeoR
             }
         }
         return ElytraCompat.getDefaultTexture(livingEntity.getItemStackFromSlot(EntityEquipmentSlot.CHEST).getItem());
-    }
-
-    protected static boolean translateToElytra(GeoModel geoModel) {
-        return RenderUtils.prepMatrixForLocator(geoModel.elytraBones);
     }
 }
--- src/main/java/com/elfmcys/yesstevemodel/client/renderer/layer/CustomPlayerItemInHandLayer.java
+++ src/main/java/com/elfmcys/yesstevemodel/client/renderer/layer/CustomPlayerItemInHandLayer.java
@@ -3,6 +3,7 @@ package com.elfmcys.yesstevemodel.client.renderer.layer;
 import com.elfmcys.yesstevemodel.geckolib3.core.util.Color;
 import com.elfmcys.yesstevemodel.geckolib3.geo.GeoLayerRenderer;
 import com.elfmcys.yesstevemodel.geckolib3.geo.IGeoRenderer;
+import com.elfmcys.yesstevemodel.geckolib3.geo.render.built.GeoBone;
 import com.elfmcys.yesstevemodel.geckolib3.geo.render.built.GeoModel;
 import com.elfmcys.yesstevemodel.geckolib3.util.RenderUtils;
 import net.minecraft.client.Minecraft;
@@ -13,6 +14,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumHandSide;
 
 import javax.annotation.Nonnull;
+import java.util.List;
 
 /**
  * 可参考原版实现 {@link net.minecraft.client.renderer.entity.layers.LayerHeldItem}。
@@ -26,53 +28,34 @@ public class CustomPlayerItemInHandLayer<T extends EntityLivingBase, R extends I
 
     @Override
     public void render(@Nonnull T entityLivingBaseIn, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch, Color renderColor) {
-        if (this.entityRenderer.getGeoModel() == null) {
-            return;
-        }
-        ItemStack offhandItem = entityLivingBaseIn.getHeldItemOffhand();
-        ItemStack mainHandItem = entityLivingBaseIn.getHeldItemMainhand();
         GeoModel geoModel = this.entityRenderer.getGeoModel();
-        if (!offhandItem.isEmpty() || !mainHandItem.isEmpty()) {
-            if (!geoModel.rightHandBones.isEmpty()) {
-                GlStateManager.pushMatrix();
-                this.renderArmWithItem(entityLivingBaseIn, mainHandItem, ItemCameraTransforms.TransformType.THIRD_PERSON_RIGHT_HAND, EnumHandSide.RIGHT);
-                GlStateManager.popMatrix();
-            }
-            if (!geoModel.leftHandBones.isEmpty()) {
-                GlStateManager.pushMatrix();
-                this.renderArmWithItem(entityLivingBaseIn, offhandItem, ItemCameraTransforms.TransformType.THIRD_PERSON_LEFT_HAND, EnumHandSide.LEFT);
-                GlStateManager.popMatrix();
-            }
-//            if (Loader.isModLoaded(TAC_ID) && TacGunRenderer.isGun(offhandItem)) {
-//                GlStateManager.pushMatrix();
-//                TacGunRenderer.renderOffhandGun(offhandItem, geoModel, entityLivingBaseIn, packedLightIn, partialTicks);
-//                GlStateManager.popMatrix();
-//            }
-
-            GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
-        }
+        if (geoModel == null) return;
+        renderArmWithItem(entityLivingBaseIn, geoModel.rightHandBones, ItemCameraTransforms.TransformType.THIRD_PERSON_RIGHT_HAND, EnumHandSide.RIGHT);
+        renderArmWithItem(entityLivingBaseIn, geoModel.leftHandBones, ItemCameraTransforms.TransformType.THIRD_PERSON_LEFT_HAND, EnumHandSide.LEFT);
+//        if (Loader.isModLoaded(TAC_ID) && TacGunRenderer.isGun(offhandItem)) {
+//            GlStateManager.pushMatrix();
+//            TacGunRenderer.renderOffhandGun(offhandItem, geoModel, entityLivingBaseIn, packedLightIn, partialTicks);
+//            GlStateManager.popMatrix();
+//        }
+        GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
     }
 
-    protected void renderArmWithItem(EntityLivingBase livingEntity, ItemStack itemStack, ItemCameraTransforms.TransformType transformType, EnumHandSide arm) {
-        if (!itemStack.isEmpty()) {
-            boolean isLeftHand = arm == EnumHandSide.LEFT;
-            // 缩放不为 0 才会渲染
-            boolean scaleResult = translateToHand(arm, this.entityRenderer.getGeoModel());
-            if (!scaleResult) {
-//                if (Loader.isModLoaded(TAC_ID) && TacGunRenderer.isGun(itemStack)) {
-//                    if (!isLeftHand) {
-//                        TacGunRenderer.renderMainhandGun(itemStack, livingEntity, light, partialTicks);
-//                    }
-//                } else {
-                GlStateManager.translate(0, -0.0625, -0.1);
-                GlStateManager.rotate(-90.0F, 1, 0, 0);
-                Minecraft.getMinecraft().getItemRenderer().renderItemSide(livingEntity, itemStack, transformType, isLeftHand);
-//                }
-            }
-        }
-    }
+    protected static void renderArmWithItem(EntityLivingBase livingEntity, List<GeoBone> bones, ItemCameraTransforms.TransformType transformType, EnumHandSide arm) {
+        if (bones.isEmpty()) return;
+        boolean isLeftHand = arm == EnumHandSide.LEFT;
+        // 先这样处理，YSM 定位组直接就叫主副手，不好改
+        //ItemStack itemStack = livingEntity.getPrimaryHand() == arm ? livingEntity.getHeldItemMainhand() : livingEntity.getHeldItemOffhand();
+        ItemStack itemStack = isLeftHand ? livingEntity.getHeldItemOffhand() : livingEntity.getHeldItemMainhand();
+        if (itemStack.isEmpty()) return;
 
-    protected static boolean translateToHand(EnumHandSide arm, GeoModel geoModel) {
-        return RenderUtils.prepMatrixForLocator(arm == EnumHandSide.LEFT ? geoModel.leftHandBones : geoModel.rightHandBones);
+        GlStateManager.pushMatrix();
+        // 缩放不为 0 才会渲染
+        boolean scaleResult = RenderUtils.prepMatrixForLocator(bones);
+        if (!scaleResult) {
+            GlStateManager.translate(0, -0.0625, -0.1);
+            GlStateManager.rotate(-90.0F, 1, 0, 0);
+            Minecraft.getMinecraft().getItemRenderer().renderItemSide(livingEntity, itemStack, transformType, isLeftHand);
+        }
+        GlStateManager.popMatrix();
     }
 }
