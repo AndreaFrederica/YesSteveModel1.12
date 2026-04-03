87179c9db7e64dc2605939836f8a0a9c9f5e69b4
RuiXuqi <90179819+RuiXuqi@users.noreply.github.com>
2026-04-03 23:04:32 +0800

    Update CustomPlayerElytraLayer.java

--- src/main/java/com/elfmcys/yesstevemodel/client/renderer/layer/CustomPlayerElytraLayer.java
+++ src/main/java/com/elfmcys/yesstevemodel/client/renderer/layer/CustomPlayerElytraLayer.java
@@ -15,6 +15,7 @@ import net.minecraft.client.renderer.entity.layers.LayerArmorBase;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EnumPlayerModelParts;
 import net.minecraft.inventory.EntityEquipmentSlot;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 
@@ -45,12 +46,12 @@ public class CustomPlayerElytraLayer<T extends EntityLivingBase, R extends IGeoR
         // 缩放不为 0 才会渲染
         if (!scaleResult) {
             GlStateManager.rotate(180, 0, 0, 1);
+            ItemStack stack = livingEntity.getItemStackFromSlot(EntityEquipmentSlot.CHEST);
             Minecraft mc = Minecraft.getMinecraft();
-            mc.getTextureManager().bindTexture(getElytraTexture(livingEntity));
+            mc.getTextureManager().bindTexture(getElytraTexture(livingEntity, stack.getItem()));
             final float scale = 1 / 16F;
             this.elytraModel.setRotationAngles(pLimbSwing, pLimbSwingAmount, pAgeInTicks, pNetHeadYaw, pHeadPitch, scale, livingEntity);
             this.elytraModel.render(livingEntity, pLimbSwing, pLimbSwingAmount, pAgeInTicks, pNetHeadYaw, pHeadPitch, scale);
-            ItemStack stack = livingEntity.getItemStackFromSlot(EntityEquipmentSlot.CHEST);
             if (stack.hasEffect()) {
                 RenderPlayer renderer = mc.getRenderManager().getSkinMap().get("default");
                 LayerArmorBase.renderEnchantedGlint(renderer, livingEntity, this.elytraModel, pLimbSwing, pLimbSwingAmount, pPartialTicks, pAgeInTicks, pNetHeadYaw, pHeadPitch, scale);
@@ -62,7 +63,7 @@ public class CustomPlayerElytraLayer<T extends EntityLivingBase, R extends IGeoR
         GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
     }
 
-    protected static ResourceLocation getElytraTexture(EntityLivingBase livingEntity) {
+    protected static ResourceLocation getElytraTexture(EntityLivingBase livingEntity, Item elytra) {
         if (livingEntity instanceof AbstractClientPlayer player) {
             if (player.isPlayerInfoSet() && player.getLocationElytra() != null) {
                 return player.getLocationElytra();
@@ -70,6 +71,6 @@ public class CustomPlayerElytraLayer<T extends EntityLivingBase, R extends IGeoR
                 return player.getLocationCape();
             }
         }
-        return ElytraCompat.getDefaultTexture(livingEntity.getItemStackFromSlot(EntityEquipmentSlot.CHEST).getItem());
+        return ElytraCompat.getDefaultTexture(elytra);
     }
 }
